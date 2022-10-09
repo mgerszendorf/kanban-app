1de9a50a5f1ee9f44580c4a5de639d2f02fd228a
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-10-09 14:48:41 +0200

    Delete sidebarSlidingElement from redux

--- client/src/redux/reducers/navigationElementsReducer.ts
+++ client/src/redux/reducers/navigationElementsReducer.ts
@@ -9,22 +9,3 @@ export const menuState = (state = false, action: ActionTypes) => {
       return state;
   }
 };
-
-export const sidebarSlidingElementState = (
-  state = false,
-  action: ActionTypes
-) => {
-  switch (action.type) {
-    case "TOGGLE_SIDEBAR_SLIDING_ELEMENT":
-      return !state;
-
-    case "SHOW_SIDEBAR_SLIDING_ELEMENT":
-      return true;
-
-    case "HIDE_SIDEBAR_SLIDING_ELEMENT":
-      return false;
-
-    default:
-      return state;
-  }
-};
