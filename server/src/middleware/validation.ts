52cbfa74ab04da752b83e81c89033aa216de627e
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-11-05 15:21:50 +0100

    Validation

---
+++ server/src/middleware/validation.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+const { validationResult } = require("express-validator");
+
+export const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+export const isObjectId = (value: any) =>
+  mongoose.Types.ObjectId.isValid(value);
