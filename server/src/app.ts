dcc5f62288e5451bad351a9dd20715d41bbd887d
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-11-13 20:55:26 +0100

    app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,40 @@
-console.log("Hello World!");
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose, { ConnectOptions } from "mongoose";
+import { routes } from "./routes";
+
+//Config
+const port = process.env.PORT || 8080;
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.use("/api", routes);
+
+// MongoDB connection.
+mongoose
+  .connect(process.env.MONGO_URI!, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  } as ConnectOptions)
+  .then(() => {
+    console.log("mongoDB connected");
+  })
+  .catch((err: any) => {
+    console.error("Error:", err);
+  });
+
+// Start the server
+app.listen(port);
+
+console.log(`Server started on port ${port}`);
+
+module.exports = app;
