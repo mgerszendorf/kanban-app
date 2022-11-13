514fc6e66017d17fed0761a8ca92f9f2ee9b8bb3
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-11-13 20:54:22 +0100

    Included new api in index.ts and added routing to the dashboard

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -6,6 +6,10 @@ import {
   getAllDashboard,
   getOne,
   updatePosition,
+  updateDashboard,
+  getFavourites,
+  updateFavouritePosition,
+  deleteDashboard,
 } from "../controllers/dashboard";
 import { validate, isObjectId } from "../middleware/validation";
 
@@ -14,14 +18,38 @@ export const dashboard = Router();
 dashboard.post("/", verifyToken, createDashboard);
 dashboard.get("/", verifyToken, getAllDashboard);
 dashboard.put("/", verifyToken, updatePosition);
+dashboard.get("/favourites", verifyToken, getFavourites);
+dashboard.put("/favourites", verifyToken, updateFavouritePosition);
 dashboard.get(
   "/:dashboardId",
-  param("dashboardId").custom((value) => {
+  param("dashboardId").custom((value: string) => {
     if (!isObjectId(value)) {
-      return Promise.reject("invalid id");
+      return Promise.reject("wrong dashboard id");
     } else return Promise.resolve();
   }),
   validate,
   verifyToken,
   getOne
+);
+dashboard.put(
+  "/:dashboardId",
+  param("dashboardId").custom((value: string) => {
+    if (!isObjectId(value)) {
+      return Promise.reject("wrong dashboard id");
+    } else return Promise.resolve();
+  }),
+  validate,
+  verifyToken,
+  updateDashboard
+);
+dashboard.delete(
+  "/:dashboardId",
+  param("dashboardId").custom((value: string) => {
+    if (!isObjectId(value)) {
+      return Promise.reject("wrong dashboard id");
+    } else return Promise.resolve();
+  }),
+  validate,
+  verifyToken,
+  deleteDashboard
 );
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { dashboard } from "./dashboard";
+import { section } from "./section";
+import { task } from "./task";
 
 export const routes = express.Router();
 
 routes.use("/dashboards", dashboard);
+routes.use("/dashboards/:dashboardId/sections", section);
+routes.use("/dashboards/:dashboardId/tasks", task);
