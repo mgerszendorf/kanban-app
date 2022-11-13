32a6a251607b874a2c655808d907660fcc1c739b
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-11-13 20:56:12 +0100

    Added types to Request

---
+++ server/@types/express/index.d.ts
@@ -0,0 +1,24 @@
+declare namespace Express {
+  export interface Request {
+    user: {
+      name: string;
+      iss: string;
+      aud: string;
+      auth_time: number;
+      user_id: string;
+      sub: string;
+      iat: number;
+      exp: number;
+      email: string;
+      email_verified: boolean;
+      uid: string;
+    };
+  }
+}
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    MONGO_URI: string;
+    PORT: number;
+  }
+}
