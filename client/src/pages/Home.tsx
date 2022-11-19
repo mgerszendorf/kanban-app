bcf7bfc934017a77726f74474f02916ddbb9efe4
Marek Gerszendorf <marek.gerszendorf@wp.pl>
2022-11-19 19:12:56 +0100

    Added Home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,46 +1,44 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import dashboardApi from "../api/dashboardApi";
-import { setGuestDashboards } from "../redux/features/guestDashboardSlice";
-import { Store } from "../redux/types";
+import Loader from "../components/Loader";
+import { setDashboards } from "../redux/features/dashboardSlice";
 
 function Home() {
-  const authState = useSelector((state: Store) => state.authState);
+  const [loader, setLoader] = useState(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const createDashboard = async () => {
-    if (authState?.user) {
-      try {
-        const res = await dashboardApi.create();
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      dispatch(
-        setGuestDashboards({
-          user: "guest",
-          icon: "📃",
-          title: "Untitle",
-          description:
-            "Please add a description here. If you need it, your description can be…",
-        })
-      );
+    try {
+      setLoader(true);
+      const res = await dashboardApi.create();
+      dispatch(setDashboards([res]));
+      navigate(`/dashboards/${res.id}}`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoader(false);
     }
   };
 
+  if (loader) return <Loader />;
+
   return (
-    <section className="home">
-      <div className="first-dashboard-wrapper">
-        <p>
-          You are currently in guest mode. If you want your boards to be saved
-          please sign in or create an account.
-        </p>
-        <button className="first-dashboard" onClick={createDashboard}>
-          Create your first dashboard
-        </button>
-      </div>
-    </section>
+    <>
+      <section className="home">
+        <div className="first-dashboard-wrapper">
+          <p>
+            You are currently in guest mode. If you want your boards to be saved
+            please sign in or create an account.
+          </p>
+          <button className="first-dashboard" onClick={createDashboard}>
+            Create your first dashboard
+          </button>
+        </div>
+      </section>
+    </>
   );
 }
 
